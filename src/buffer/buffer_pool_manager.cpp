167548bc7b647aff6216a8f81058a366f550dbd6
ysj1173886760 <1173886760@qq.com>
2021-09-21 14:58:13 +0800

    fix buffer pool manager

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -100,10 +100,8 @@ bool BufferPoolManager::FlushPageImpl(page_id_t page_id) {
   }
 
   frame_id_t frame_id = page_table_[page_id];
-  if (pages_[frame_id].is_dirty_) {
-    disk_manager_->WritePage(pages_[frame_id].GetPageId(), pages_[frame_id].GetData());
-    pages_[frame_id].is_dirty_ = false;
-  }
+  disk_manager_->WritePage(pages_[frame_id].GetPageId(), pages_[frame_id].GetData());
+  pages_[frame_id].is_dirty_ = false;
 
   return true;
 }
@@ -153,7 +151,6 @@ bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
   // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
   // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
   std::lock_guard<std::mutex> lck(latch_);
-  disk_manager_->DeallocatePage(page_id);
   if (page_table_.count(page_id) == 0) {
     return true;
   }
@@ -163,8 +160,10 @@ bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
     return false;
   }
 
+  disk_manager_->DeallocatePage(page_id);
   page_table_.erase(page_id);
   free_list_.push_front(frame_id);
+  replacer_->Pin(frame_id);
   return true;
 }
 
@@ -172,10 +171,11 @@ void BufferPoolManager::FlushAllPagesImpl() {
   // You can do it!
   std::lock_guard<std::mutex> lck(latch_);
   for (size_t i = 0; i < pool_size_; i++) {
-    if (pages_[i].is_dirty_) {
-      disk_manager_->WritePage(pages_[i].GetPageId(), pages_[i].GetData());
-      pages_[i].is_dirty_ = false;
+    if (page_table_.count(pages_[i].GetPageId()) == 0) {
+      continue;
     }
+    disk_manager_->WritePage(pages_[i].GetPageId(), pages_[i].GetData());
+    pages_[i].is_dirty_ = false;
   }
 }
 
