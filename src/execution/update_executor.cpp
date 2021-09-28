6b401fa63c84a3551594fd47f053479ebb8ace29
ysj1173886760 <1173886760@qq.com>
2021-09-28 19:55:59 +0800

    implement update executor

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -17,9 +17,29 @@ namespace bustub {
 
 UpdateExecutor::UpdateExecutor(ExecutorContext *exec_ctx, const UpdatePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),
+    plan_(plan),
+    table_info_(exec_ctx->GetCatalog()->GetTable(plan->TableOid())),
+    child_executor_(std::move(child_executor)),
+    index_list_(exec_ctx->GetCatalog()->GetTableIndexes(table_info_->name_)) {}
 
-void UpdateExecutor::Init() {}
 
-bool UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) { return false; }
+void UpdateExecutor::Init() {
+    child_executor_->Init();
+}
+
+bool UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
+    if (child_executor_->Next(tuple, rid)) {
+        Tuple newTuple = GenerateUpdatedTuple(*tuple);
+        for (const auto &index : index_list_) {
+            Tuple oldKey(tuple->KeyFromTuple(table_info_->schema_, index->key_schema_, index->index_->GetKeyAttrs()));
+            Tuple newKey(newTuple.KeyFromTuple(table_info_->schema_, index->key_schema_, index->index_->GetKeyAttrs()));
+            index->index_->DeleteEntry(oldKey, *rid, exec_ctx_->GetTransaction());
+            index->index_->InsertEntry(newKey, *rid, exec_ctx_->GetTransaction());
+        }
+        return true;
+    }
+
+    return false;
+}
 }  // namespace bustub
