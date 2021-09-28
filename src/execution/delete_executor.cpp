fedb72d5f29d1b2c716ce4511fb02932ed94b3d8
ysj1173886760 <1173886760@qq.com>
2021-09-28 20:22:15 +0800

    implement delete executor

--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -17,10 +17,31 @@ namespace bustub {
 
 DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),
+    plan_(plan),
+    child_executor_(std::move(child_executor)),
+    metatable_(exec_ctx->GetCatalog()->GetTable(plan->TableOid())),
+    index_list_(exec_ctx->GetCatalog()->GetTableIndexes(metatable_->name_)) {}
 
-void DeleteExecutor::Init() {}
+void DeleteExecutor::Init() {
+    child_executor_->Init();
+}
 
-bool DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) { return false; }
+bool DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
+    if (child_executor_->Next(tuple, rid)) {
+        if (metatable_->table_->MarkDelete(*rid, exec_ctx_->GetTransaction())) {
+            for (const auto &index : index_list_) {
+                Tuple key(tuple->KeyFromTuple(metatable_->schema_, index->key_schema_, index->index_->GetKeyAttrs()));
+                index->index_->DeleteEntry(key, *rid, exec_ctx_->GetTransaction());
+            }
+        } else {
+            throw Exception("failed to delete");
+        }
+
+        return true;
+    }
+
+    return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/delete_executor.h
+++ src/include/execution/executors/delete_executor.h
@@ -51,5 +51,7 @@ class DeleteExecutor : public AbstractExecutor {
   const DeletePlanNode *plan_;
   /** The child executor to obtain rid from. */
   std::unique_ptr<AbstractExecutor> child_executor_;
+  TableMetadata *metatable_;
+  std::vector<IndexInfo *> index_list_;
 };
 }  // namespace bustub
