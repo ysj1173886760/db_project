1892386aa8a893169ac3430139a34b86ace21163
ysj1173886760 <1173886760@qq.com>
2021-09-29 09:13:36 +0800

    implement nested index join

--- src/execution/nested_index_join_executor.cpp
+++ src/execution/nested_index_join_executor.cpp
@@ -29,6 +29,28 @@ void NestIndexJoinExecutor::Init() {
 bool NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) {
     Tuple outerTuple;
     RID outerRID;
+    std::vector<RID> result;
+    const auto *outer_schema = plan_->OuterTableSchema();
+    const auto *inner_schema = plan_->InnerTableSchema();
+    const auto *output_schema = plan_->OutputSchema();
+    while (child_executor_->Next(&outerTuple, &outerRID)) {
+        Tuple key(outerTuple.KeyFromTuple(*outer_schema, index_->key_schema_, index_->index_->GetKeyAttrs()));
+        index_->index_->ScanKey(key, &result, exec_ctx_->GetTransaction());
+        if (!result.empty()) {
+            Tuple innerTuple;
+            if (metatable_->table_->GetTuple(result[0], &innerTuple, exec_ctx_->GetTransaction())) {
+                std::vector<Column> cols = output_schema->GetColumns();
+                std::vector<Value> vals(output_schema->GetColumnCount());
+                for (uint i = 0; i < vals.size(); i++) {
+                    vals[i] = cols[i].GetExpr()->EvaluateJoin(&outerTuple, outer_schema, &innerTuple, inner_schema);
+                }
+                *tuple = Tuple(vals, output_schema);
+                return true;
+            } else {
+                throw Exception("failed to get tuple");
+            }
+        }
+    }
     return false;
 }
 
